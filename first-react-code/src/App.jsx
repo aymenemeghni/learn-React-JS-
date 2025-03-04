2a0c88479e823921e83fa2bfb871de056ef0aaed
AYMENE <meghnimohamedaymene@gmail.com>
2025-03-04 15:08:40 +0100

    new update

--- first-react-code/src/App.jsx
+++ first-react-code/src/App.jsx
@@ -3,11 +3,22 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [x, setx] = useState(1);
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            if (x === 1) {
+              setx(0);
+              setCount((count) => count + 1);
+            } else {
+              setCount((count) => count - 1);
+              setx(1);
+            }
+          }}
+        >
           count is {count}
         </button>
       </div>
