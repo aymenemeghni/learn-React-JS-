fe0317af5c4798c6da381c293651b346207a5b83
AYMENE <meghnimohamedaymene@gmail.com>
2025-03-06 11:06:44 +0100

    new update

--- first-react-code/src/App.jsx
+++ first-react-code/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-     <h1 style={{color :"red"}}>fffffffffff</h1>
+      <h1 style={{color :"red"}}>fffffffffff</h1>
       <div id="head">
         <Head />
       </div>
